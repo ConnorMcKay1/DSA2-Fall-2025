1277fe323ce2450114b8695eeebcde7dc1b994b6
Connor <mckayc1@go.stockton.edu>
2025-12-10 14:54:22 -0500

    Initializing

---
+++ Sequence Alignment.py
@@ -0,0 +1,13 @@
+print("whoop whoop")
+
+text = input("Enter the text you want to seach: ")
+print('\t' + "===>" + text)
+print('\n'+'\n')
+pattern = input("Enter What you're looking for: ")
+print('\t' + "===>" + pattern)
+
+
+def Alignment(text, pattern, Alpha, Delta):
+    array = []
+    print(array)
+
